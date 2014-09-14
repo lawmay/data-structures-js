93627fd97e4b9099b2a9b27769124df4099709cc
LM <lmay01@gmail.com>
2014-09-13 23:26:24 -0700

    [setup] linked list structure

---
+++ LinkedList.js
@@ -0,0 +1,24 @@
+/*
+ * Linked List
+ * 
+ * 
+ *
+ */
+
+
+var LinkedList = function() {
+};
+
+LinkedList.prototype.addToTail = function() {
+};
+
+LinkedList.prototype.removeHead = function() {
+};
+
+LinkedList.prototype.contains = function() {
+};
+
+LinkedList.prototype.makeNode = function() {
+};
+
+
