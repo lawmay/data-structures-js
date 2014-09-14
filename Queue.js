df975d4620ec6e7e92468e2a0f4b59e8a9300c13
LM <lmay01@gmail.com>
2014-09-13 23:28:57 -0700

    [setup] queue structure

---
+++ Queue.js
@@ -0,0 +1,20 @@
+/*
+ * Queue
+ * 
+ * 
+ *
+ */
+
+
+var Queue = function() {
+};
+
+Queue.prototype.enqueue = function(string) {
+};
+
+Queue.prototype.dequeue = function() {
+};
+
+Queue.prototype.size = function() {
+};
+
