ab316b14c3910ecdb9e103f38f13a4919f3710da
LM <lmay01@gmail.com>
2014-09-13 23:27:51 -0700

    [setup] stack structure

---
+++ Stack.js
@@ -0,0 +1,20 @@
+/*
+ * Stack
+ * 
+ * 
+ *
+ */
+
+
+var Stack = function() {
+};
+
+Stack.prototype.push = function(string) {
+};
+
+Stack.prototype.pop = function() {
+};
+
+Stack.prototype.size() = function() {
+};
+
