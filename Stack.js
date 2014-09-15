f71aec7d5f4dc9f5f2687cb153cd63a591e80efb
LM <lmay01@gmail.com>
2014-09-14 19:46:26 -0700

    [feat] basic stack implementation using js native array

--- Stack.js
+++ Stack.js
@@ -7,14 +7,17 @@
 
 
 var Stack = function() {
+  this.results = [];
 };
 
 Stack.prototype.push = function(string) {
+  this.results.push(string);
 };
 
 Stack.prototype.pop = function() {
+  return this.results.pop();
 };
 
 Stack.prototype.size = function() {
+  return this.results.length;
 };
-
