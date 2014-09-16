344f651d092b716bb40a220aa42b426ecd968477
LM <lmay01@gmail.com>
2014-09-15 19:17:03 -0700

    [refactor] basic queue implementation using native javascript array

--- Queue.js
+++ Queue.js
@@ -7,14 +7,18 @@
 
 
 var Queue = function() {
+  this.storage = [];
 };
 
 Queue.prototype.enqueue = function(string) {
+  this.storage.push(string);
 };
 
 Queue.prototype.dequeue = function() {
+  return this.storage.unshift();
 };
 
 Queue.prototype.size = function() {
+  return this.storage.size();
 };
 
